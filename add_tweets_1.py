9d3cc9d9d486ae9a088cff5f68c7ddb13206f0ba
Rohan Anand <96521078+rohan472000@users.noreply.github.com>
2023-04-04 13:49:38 +0530

    Update add_tweets_1.py

--- add_tweets_1.py
+++ add_tweets_1.py
@@ -5,11 +5,14 @@ import pandas as pd
 import os
 
 
-access_key = os.environ['ACCESS_KEY']
-access_secret =  os.environ['ACCESS_SECRET']
-consumer_key = os.environ['CONSUMER_KEY']
-consumer_secret = os.environ['CONSUMER_SECRET']
-
+# access_key = os.environ['ACCESS_KEY']
+# access_secret =  os.environ['ACCESS_SECRET']
+# consumer_key = os.environ['CONSUMER_KEY']
+# consumer_secret = os.environ['CONSUMER_SECRET']
+access_key = os.getenv("ACCESS_KEY")
+access_secret = os.getenv("ACCESS_SECRET")
+consumer_key = os.getenv("CONSUMER_KEY")
+consumer_secret = os.getenv("CONSUMER_SECRET")
 
 # Twitter authentication
 auth = tweepy.OAuthHandler(access_key, access_secret)
